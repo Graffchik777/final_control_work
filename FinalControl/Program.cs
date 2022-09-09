daea8ea76b52b01e2f25f0f4190d892a6753db2d
Graffchik777 <legione77@mail.ru>
2022-09-09 14:40:55 +0300

    add Program

---
+++ FinalControl/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+string[] array = { "A", "bB", "cCc", "dddd", "12_qaz", "3-4", "H_R" };
+
+void comparisonArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[i] = array[i]; ;
+            Console.Write($"{newArray[i]}, ");
+        }
+    }
+    Console.WriteLine();
+}
+
+void Print(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+        Console.WriteLine();
+
+}
+
+Print(array);
+comparisonArray(array);
+Console.WriteLine();
